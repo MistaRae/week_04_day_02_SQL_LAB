c1d388b661e9726b72d3595f817b7f7f45b7ae6c
MistaRae <itsamistarae@gmail.com>
2021-04-21 10:40:16 +0100

    pushed pending Colin's review

--- db/music_library.sql
+++ db/music_library.sql
@@ -9,7 +9,12 @@ CREATE TABLE artists (
 CREATE TABLE albums (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
-    genre VARCHAR(255),
+    genre VARCHAR(255), 
     artist VARCHAR(255),
     artist_id INT REFERENCES artists(id)
 );
+
+-- consider using "ON DELETE CASCADE" after the reference statement on line 14
+-- allows you to delete a table entry with dependent keys referenced in other tables
+
+-- is artist required? 
