44c9ee3b5897a6c022fa9eda3978a929ca340a47
MistaRae <itsamistarae@gmail.com>
2021-04-21 09:11:15 +0100

    fixed initial errors

--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -6,7 +6,7 @@ from models.artist import Artist
 import repositories.artist_repository as artist_repository
 
 def save(album):
-    sql = "INSERT INTO albums (title, genre, artist_id) VALUES (%s, %s, %s) RETURNING *"
+    sql = "INSERT INTO albums (title, genre, artist) VALUES (%s, %s, %s) RETURNING *"
     values = [album.title, album.genre, album.artist]
     result = run_sql(sql, values)
     id = result[0]['id']
